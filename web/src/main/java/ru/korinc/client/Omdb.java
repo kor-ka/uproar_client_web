8e1a7813989e5af7bafb506505f121532f2fa9ec
gputintsev <g.putintsev@qiwi.ru>
2017-12-19 20:48:13 +0300

    feat(web) get balance wia metamask

--- web/src/main/java/ru/korinc/client/Omdb.java
+++ web/src/main/java/ru/korinc/client/Omdb.java
@@ -8,19 +8,13 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 import ru.korinc.core.AppCore;
 import ru.korinc.core.Model;
-import ru.korinc.core.entity.Load;
-import ru.korinc.core.entity.Movie;
-import ru.korinc.core.modules.Foo;
-import ru.korinc.shared.FieldVerifier;
+import ru.korinc.runtime.interop.web3.Eth;
 
 import static ru.korinc.core.utils.Utils.apply;
 
@@ -47,121 +41,86 @@ public class Omdb implements EntryPoint {
 
     private int generation = -1;
 
+    private RootPanel mRetryContainer;
+
+    private Button mRetry;
+
+
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        final Button sendButton = new Button("Send");
-        final TextBox nameField = new TextBox();
+        mRetry = new Button("Retry");
+        final TextBox balanceField = new TextBox();
+        balanceField.setEnabled(false);
+        // We can add style names to widgets
+        mRetry.addStyleName("sendButton");
 
-        nameField.setText("Star wars");
+
         final Label errorLabel = new Label();
 
-        // We can add style names to widgets
-        sendButton.addStyleName("sendButton");
 
-        // Add the nameField and sendButton to the RootPanel
+
+        // Add the balanceField and sendButton to the RootPanel
         // Use RootPanel.get() to get the entire body element
-        RootPanel.get("nameFieldContainer").add(nameField);
-        RootPanel labelListContainer = RootPanel.get("labelListContainer");
+        RootPanel.get("nameFieldContainer").add(balanceField);
+        mRetryContainer = RootPanel.get("retryButtonContainer");
         RootPanel.get("errorLabelContainer").add(errorLabel);
 
-        // Focus the cursor on the name field when the app loads
-        nameField.setFocus(true);
-        nameField.selectAll();
 
-        // Create the popup dialog box
-        final DialogBox dialogBox = new DialogBox();
-        dialogBox.setText("Remote Procedure Call");
-        dialogBox.setAnimationEnabled(true);
-        final Button closeButton = new Button("Close");
-        // We can set the id of a widget by accessing its Element
-        closeButton.getElement().setId("closeButton");
-        final Label textToServerLabel = new Label();
-        final HTML serverResponseLabel = new HTML();
-        VerticalPanel dialogVPanel = new VerticalPanel();
-        dialogVPanel.addStyleName("dialogVPanel");
-        dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-        dialogVPanel.add(textToServerLabel);
-        dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-        dialogVPanel.add(serverResponseLabel);
-        dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-        dialogVPanel.add(closeButton);
-        dialogBox.setWidget(dialogVPanel);
-
-        // Add a handler to close the DialogBox
-        closeButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                dialogBox.hide();
-                sendButton.setEnabled(true);
-                sendButton.setFocus(true);
-            }
-        });
-
-        // Create a handler for the sendButton and nameField
+        // Create a handler for the sendButton and balanceField
         class MyHandler implements ClickHandler, KeyUpHandler {
 
             /**
              * Fired when the user clicks on the sendButton.
              */
             public void onClick(ClickEvent event) {
-                sendNameToServer();
+                retry();
             }
 
             /**
-             * Fired when the user types in the nameField.
+             * Fired when the user types in the balanceField.
              */
             public void onKeyUp(KeyUpEvent event) {
                 if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                    sendNameToServer();
+                    retry();
                 }
             }
 
             /**
-             * Send the name from the nameField to the server and wait for a response.
+             * Send the name from the balanceField to the server and wait for a response.
              */
-            private void sendNameToServer() {
-                // First, we validate the input.
-                errorLabel.setText("");
-                String textToServer = nameField.getText();
-                if (!FieldVerifier.isValidName(textToServer)) {
-                    errorLabel.setText("Please enter at least three characters");
-                    return;
-                }
+            private void retry() {
 
-
-                model.searchMovieByTitleQuery(textToServer);
+                checkBalance(balanceField);
             }
         }
 
+        checkBalance(balanceField);
+
+
+
         // Add a handler to send the name to the server
         MyHandler handler = new MyHandler();
-        sendButton.addClickHandler(handler);
-        nameField.addKeyUpHandler(handler);
-
-        model.getSearchResults().subscribe(searchEntities -> {
-            labelListContainer.clear(false);
-
-            apply(searchEntities, (iterator, val) -> {
-
-                if (val instanceof Movie) {
-                    Label l = new Label(((Movie) val).getTitle());
-                    labelListContainer.add(l);
-                } else if (val instanceof Load) {
-                    Button b = new Button("Load more");
-                    b.addClickHandler(new ClickHandler() {
-                        @Override
-                        public void onClick(ClickEvent event) {
-                            model.searchMovieByTitleQuery(((Load) val).getQuery());
-                            b.setEnabled(false);
-                            b.setText("loading...");
-                        }
-                    });
-                    labelListContainer.add(b);
-                }
+        mRetry.addClickHandler(handler);
+        balanceField.addKeyUpHandler(handler);
 
 
-            });
-        });
+
+
+
+    }
+
+    private void checkBalance(TextBox nameField) {
+        mRetryContainer.clear();
+
+        if(new Eth().loggedIn()){
+            new Eth().getBalance(balance -> nameField.setText(balance + ""));
+        }else{
+            nameField.setText("login first");
+            mRetryContainer.add(mRetry);
+        }
+
+
     }
 }
---
+++ web/src/main/java/ru/korinc/runtime/interop/web3/Eth.java
@@ -0,0 +1,38 @@
+package ru.korinc.runtime.interop.web3;
+
+import jsinterop.annotations.JsType;
+
+/**
+ * Created by gputintsev on 19.12.17.
+ */
+
+@JsType(name = "eth")
+public class Eth {
+
+    public Eth() {
+    }
+
+    public native void getBalance(BalanceCallback callback)/*-{
+        var coinbase = $wnd.web3.eth.coinbase
+        console.log(coinbase)
+        var balance = $wnd.web3.eth.getBalance(coinbase, function(error, result){
+            if(!error){
+
+                callback.@ru.korinc.runtime.interop.web3.Eth.BalanceCallback::onBalance(Ljava/lang/Double;)(result);
+                console.log(result)
+            }else{
+                console.error(error);
+                }
+        })
+   }-*/;
+
+   public native boolean loggedIn()/*-{
+        var coinbase = $wnd.web3.eth.coinbase
+       return coinbase!=null;
+   }-*/;
+
+    public interface BalanceCallback {
+
+        void onBalance(Double balance);
+    }
+}
